7832a236db4b77f1c8eb033ac3a681cffcee6086
WorldSportStars <tvkonflikt@gmail.com>
2025-07-03 05:42:58 +0200

    fourth commit

--- app/.well-known/farcaster.json/route.ts
+++ app/.well-known/farcaster.json/route.ts
@@ -16,42 +16,21 @@ export async function GET() {
 
   return Response.json({
     accountAssociation: {
-      "accountAssociation": {
-        "header": "",
-        "payload": "",
-        "signature": ""
-      },
-      "frame": {
-        "version": "1",
-        "name": "HowToBase Academy",
-        "iconUrl": "https://how-to-base.vercel.app//icon.png",
-        "splashImageUrl": "https://how-to-base.vercel.app//splash.png",
-        "splashBackgroundColor": "#000000",
-        "homeUrl": "https://how-to-base.vercel.app/",
-        "webhookUrl": "https://how-to-base.vercel.app//api/webhook",
-        "heroImageUrl": "https://how-to-base.vercel.app//hero.png",
-        "ogTitle": "HowToBase",
-        "ogImageUrl": "https://how-to-base.vercel.app//hero.png"
-      }
-    }
-    frame: withValidProperties({
+      header: "eyJmaWQiOjM0MDY3OCwidHlwZSI6ImF1dGgiLCJrZXkiOiIweDc2MjExZGEyNEFFZGFmOTEzQTc3OTVFODE0RTU5MDExY2RCQzQyNjQifQ",
+      payload: "eyJkb21haW4iOiJob3ctdG8tYmFzZS52ZXJjZWwuYXBwIn0",
+      signature: "ajpO9rZapfYXjiUCMHuFf5QPngS3IJ0S6kMw/AiOPS4ak9Qo2hygriJNuQVe14VXL8tA6Kgu5XqTHYFskcULMRw="
+    },
+    frame: {
       version: "1",
-      name: process.env.NEXT_PUBLIC_ONCHAINKIT_PROJECT_NAME,
-      subtitle: process.env.NEXT_PUBLIC_APP_SUBTITLE,
-      description: process.env.NEXT_PUBLIC_APP_DESCRIPTION,
-      screenshotUrls: [],
-      iconUrl: process.env.NEXT_PUBLIC_APP_ICON,
-      splashImageUrl: process.env.NEXT_PUBLIC_APP_SPLASH_IMAGE,
-      splashBackgroundColor: process.env.NEXT_PUBLIC_SPLASH_BACKGROUND_COLOR,
-      homeUrl: URL,
-      webhookUrl: `${URL}/api/webhook`,
-      primaryCategory: process.env.NEXT_PUBLIC_APP_PRIMARY_CATEGORY,
-      tags: [],
-      heroImageUrl: process.env.NEXT_PUBLIC_APP_HERO_IMAGE,
-      tagline: process.env.NEXT_PUBLIC_APP_TAGLINE,
-      ogTitle: process.env.NEXT_PUBLIC_APP_OG_TITLE,
-      ogDescription: process.env.NEXT_PUBLIC_APP_OG_DESCRIPTION,
-      ogImageUrl: process.env.NEXT_PUBLIC_APP_OG_IMAGE,
-    }),
+      name: "HowToBase Academy",
+      iconUrl: "https://how-to-base.vercel.app/icon.png",
+      splashImageUrl: "https://how-to-base.vercel.app/splash.png",
+      splashBackgroundColor: "#000000",
+      homeUrl: "https://how-to-base.vercel.app/",
+      webhookUrl: "https://how-to-base.vercel.app/api/webhook",
+      heroImageUrl: "https://how-to-base.vercel.app/hero.png",
+      ogTitle: "HowToBase",
+      ogImageUrl: "https://how-to-base.vercel.app/hero.png"
+    }
   });
 }
